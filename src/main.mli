1e2adafc14d1a6f62c21cb136ca0a19f23af3bb7
Dario Halilovic <dario.halilovic@epfl.ch>
2026-03-18 16:33:32 +0100

    fix(docs): put API documentation below function declaration

--- src/main.mli
+++ src/main.mli
@@ -1,9 +1,9 @@
-(** Run the given OCaml file in the current Rocq context. *)
 val run_file : string -> unit
+(** [run_file file] runs the given OCaml file in the current Rocq context. *)
 
-(** Run the given OCaml snippet in the current Rocq context. *)
 val run_snippet : loc:Loc.t -> string -> unit
+(** [run_snippet snippet] runs the given OCaml snippet in the current Rocq context. *)
 
-(** Run the given OCaml snippet in the current Rocq context,
-    returning the output term. *)
 val run_snippet_as_term : loc:Loc.t -> string -> Constrexpr.constr_expr_r
+(** [run_snippet_as_term ~loc snippet] runs the given OCaml snippet in the current Rocq context,
+    returning the output term. *)
