a15cdf0b2d95a681f7dbc6968b155067252749fa
Dario Halilovic <dario.halilovic@epfl.ch>
2026-03-27 16:13:11 +0100

    chore(docs): improve documentation of Scaffold

--- src/scaffold.mli
+++ src/scaffold.mli
@@ -1,4 +1,12 @@
-(** Methods for constructing the scaffold over a snippet. *)
+(** Methods for constructing scaffolds over a snippet. *)
+
+(** {1 Scaffolds} *)
+
+(** A scaffold encapsulates a [Snippet.t] by wrapping it between
+    initialization and finalization code.
+
+    Scaffolds are used to transform snippets before they run, adding
+    the necessary boilerplate. *)
 
 (** Type of scaffolds. *)
 type t
@@ -6,8 +14,10 @@ type t
 (** Create a scaffold for the given snippet. *)
 val make : Snippet.t -> t
 
-(** Wraps the main content of the scaffold with [before] and [after]. *)
+(** Wrap the main content of the scaffold, adding [before] to the end of
+    the initialization code and [after] to the start of the finalization
+    code. *)
 val wrap : before:string -> after:string -> t -> t
 
-(** Returns the content of the scaffold as a string. *)
+(** Return the contents of the scaffold as a string. *)
 val contents : t -> string
