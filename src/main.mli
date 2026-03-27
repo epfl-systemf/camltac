78e275baab580c071b7121fc5e934d7aebed457e
Dario Halilovic <dario.halilovic@epfl.ch>
2026-03-27 16:15:03 +0100

    chore(docs): improve docs in main.mli

--- src/main.mli
+++ src/main.mli
@@ -1,9 +1,11 @@
+(** Main entry point of MLtac. *)
+
+(** Run the given OCaml file in the current Rocq context. *)
 val run_file : string -> unit
-(** [run_file file] runs the given OCaml file in the current Rocq context. *)
 
+(** Runs the given OCaml snippet in the current Rocq context. *)
 val run_snippet : Snippet.t -> unit
-(** [run_snippet snippet] runs the given OCaml snippet in the current Rocq context. *)
 
-val run_snippet_as_term : Snippet.t -> Constrexpr.constr_expr_r
-(** [run_snippet_as_term snippet] runs the given OCaml snippet in the current Rocq context,
+(** Runs the given OCaml snippet in the current Rocq context,
     returning the output term. *)
+val run_snippet_as_term : Snippet.t -> Constrexpr.constr_expr_r
