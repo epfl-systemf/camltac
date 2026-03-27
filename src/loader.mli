3b478949f79f1aaf9d2efd2e6638cb7015640bdd
Dario Halilovic <dario.halilovic@epfl.ch>
2026-03-27 16:27:21 +0100

    chore(docs): put docstrings before function declaration instead of after

--- src/loader.mli
+++ src/loader.mli
@@ -1,6 +1,6 @@
 (** This file handles dynamic loading of compiled libraries
     by wrapping the [Dynlink] module from the OCaml standard library. *)
 
+(** [load_file file] loads the given compiled file into the current Rocq
+    context. The file path must be absolute. *)
 val load_file : string -> unit
-(** Loads the given compiled [file] into the current Rocq context.
-    The file path must be absolute. *)
