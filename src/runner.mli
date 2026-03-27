b02e493ba5f4557d9224ff2c7c5a44684e2f5909
Dario Halilovic <dario.halilovic@epfl.ch>
2026-03-27 16:18:29 +0100

    chore(docs): improve docs for Runner

--- src/runner.mli
+++ src/runner.mli
@@ -1,6 +1,9 @@
-val run_file : string -> unit
-(** [run_file file] compiles and runs the given OCaml file. *)
+(** Methods for running arbitrary OCaml files and snippets. *)
 
-val run_snippet : string -> unit
+(** [run_file file] compiles and runs the given OCaml file, where [file]
+    is an absolute path. *)
+val run_file : string -> unit
+
 (** [run_snippet snippet] compiles and runs the given OCaml snippet by
     creating a temporary file. *)
+val run_snippet : string -> unit
