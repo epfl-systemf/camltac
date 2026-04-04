fd3fc876f95d19f1167375609414bf3c84dd8ed7
Dario Halilovic <dario.halilovic@epfl.ch>
2026-04-04 12:36:36 +0200

    chore: remove unused run_snippet_as_term

--- src/main.ml
+++ src/main.ml
@@ -5,11 +5,3 @@ let run_file = Runner.run_file
 let run_snippet snippet =
   let scaffold = Scaffold.make snippet in
   Runner.run_code (Scaffold.contents scaffold)
-
-let run_snippet_as_term snippet =
-  let scaffold =
-    snippet
-    |> Scaffold.make
-    |> Scaffold.wrap ~before:"Runtime.Registry.register_output begin" ~after:"end" in
-  Runner.run_code (Scaffold.contents scaffold);
-  Runtime.Registry.get_last_output ()
--- src/main.mli
+++ src/main.mli
@@ -5,7 +5,3 @@ val run_file : string -> unit
 
 (** Runs the given OCaml snippet in the current Rocq context. *)
 val run_snippet : Snippet.t -> unit
-
-(** Runs the given OCaml snippet in the current Rocq context,
-    returning the output term. *)
-val run_snippet_as_term : Snippet.t -> Constrexpr.constr_expr_r
