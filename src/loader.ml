a62bcd059abb795b18e060dfd59b5b73f9063903
Dario Halilovic <dario.halilovic@epfl.ch>
2026-04-15 12:02:15 +0200

    fix: avoid "execution of module initializers in the shared library failed" message

--- src/loader.ml
+++ src/loader.ml
@@ -20,6 +20,13 @@ let load_file file =
   try
     Dynlink.loadfile_private file;
     Flags.if_verbose Feedback.msg_debug (fmt "File %s successfully loaded." file);
-  with Dynlink.Error e ->
-    let message = Dynlink.error_message e in
-    CErrors.user_err (str message)
+  with
+  | Dynlink.Error (Dynlink.Library's_module_initializers_failed exn) ->
+     (* This means that the OCaml code inputted by the user failed.
+        In this case, we want to avoid the "execution of module initializers in
+        the shared library failed" message, so we re-raise the exception and let
+        Rocq print it instead. *)
+     raise exn
+  | Dynlink.Error e ->
+     let message = Dynlink.error_message e in
+     CErrors.user_err (str message)
