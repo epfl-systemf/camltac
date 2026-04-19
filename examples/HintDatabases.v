6bc3d116d2ecb78768f1901b5b1393bd833d7d96
Dario Halilovic <dario.halilovic@epfl.ch>
2026-04-19 21:26:56 +0200

    chore(examples): update example to use HintDb API

--- examples/HintDatabases.v
+++ examples/HintDatabases.v
@@ -13,61 +13,49 @@ Require Import Ltac2.Ltac2.
 
 MLtac Run ocaml:{{
   open Tacticals
-  open Ltac_plugin
+  open Api
   open Ltac2_plugin
   open Tac2ffi
   open Tac2externals
 
   let with_hint_db dbs tacK =
+    let dbs = List.map HintDb.get_db dbs in
     (* [dbs] : list of hint databases *)
     (* [tacK] : tactic to run on a hint *)
-    Proofview.Goal.enter begin
-	fun gl ->
-	let syms = ref [] in
-	let _ =
-	  List.iter (fun l ->
-		     (* Fetch the searchtable from the database*)
-		     let db = Hints.searchtable_map l in
-		     (* iterate over the hint database, pulling the hint *)
-		     (* list out for each. *)
-		     Hints.Hint_db.iter (fun _ _ hintlist ->
-					 syms := hintlist::!syms) db) dbs in
-	(* Now iterate over the list of list of hints, *)
-	List.fold_left
-	  (fun tac hints ->
-	   List.fold_left
-	     (fun tac hint1 ->
-	      Hints.FullHint.run hint1
-		       (fun hint2 ->
-			      (* match the type of the hint to pull out the lemma *)
-			      match hint2 with
-				Hints.Give_exact h
-			      | Hints.Res_pf h
-			      | Hints.ERes_pf h ->
-                                 let _, lem = Hints.hint_as_term h in
-				 tclORELSE (tacK lem) tac
-			      | _ -> tac))
-	     tac hints)
-	  (tclFAIL (Pp.str "No applicable tactic!")) !syms
+    Proofview.Goal.enter begin fun gl ->
+      let hints = List.concat_map HintDb.all_hints dbs in
+      let tac = tclFAIL (Pp.str "No applicable tactic!") in
+      List.fold_left begin fun tac hint ->
+        HintDb.Hint.run hint begin fun hint_ast ->
+          match hint_ast with
+            Exact h
+          | Apply h
+          | EApply h ->
+             let _, lem = Hints.hint_as_term h in
+             tclORELSE (tacK lem) tac
+          | _ -> tac
+          end
+        end tac hints
       end
 
   let add_resolve_to_db lem db =
+    let db = HintDb.get_db db in
     Proofview.Goal.enter begin fun gl ->
       let sigma = Proofview.Goal.sigma gl in
       (* Tolerate applications to please tclABSTRACT in a section *)
       let lem, _ = EConstr.decompose_app sigma lem in
       match EConstr.destRef sigma lem with
       | lem, _ ->
-        let () = Hints.add_hints ~locality:Hints.Local db (Hints.HintsResolveEntry [({ Typeclasses.hint_priority = Some 1 ; Typeclasses.hint_pattern = None }, true, lem)]) in
-        tclIDTAC
+         let _ = HintDb.hint_resolve ~cost:1 lem db in
+         tclIDTAC
       | exception Constr.DestKO -> tclFAIL (Pp.str "Cannot add non-global to hint database")
-    end
+      end
 
-  let () = Runtime.Registry.register_ltac2 "add_resolve_to_db" (constr @-> list string @-> tac unit) add_resolve_to_db
+  let () = Runtime.Registry.register_ltac2 "add_resolve_to_db" (constr @-> string @-> tac unit) add_resolve_to_db
   let () = Runtime.Registry.register_ltac2 "with_hint_db" (list string @-> fun1 constr unit @-> tac unit) with_hint_db
 }}.
 
-Ltac2 @external add_to_dbs : constr -> string list -> unit := "mltac.plugin.runtime" "add_resolve_to_db".
+Ltac2 @external add_to_db : constr -> string -> unit := "mltac.plugin.runtime" "add_resolve_to_db".
 Ltac2 @external with_hint_db : string list -> (constr -> unit) -> unit := "mltac.plugin.runtime" "with_hint_db".
 
 (*|
@@ -79,6 +67,6 @@ Create HintDb foo.
 Axiom dummy_lemma : 3 = 5.
 
 Goal 3 = 5.
-  add_to_dbs constr:(dummy_lemma) ["foo"].
+  add_to_db constr:(dummy_lemma) "foo".
   auto with foo.
 Qed.
