affa33c7385950685aa93389b66b966b64c059e4
Dario Halilovic <dario.halilovic@epfl.ch>
2026-04-19 21:23:39 +0200

    feat: create Tactics API

---
+++ src/api/tactics.ml
@@ -0,0 +1,27 @@
+(** Standard tactic syntax. *)
+
+(** {1 Tactic monad} *)
+
+type 'a tactic = 'a Proofview.tactic
+
+let return = Proofview.Monad.return
+let (let*) = Proofview.Monad.(>>=)
+let fail = Proofview.tclZERO
+
+exception More_than_one_goal
+(** Exception thrown when there is more than one goal in focus. *)
+
+let with_env f =
+  let* goals = Proofview.Goal.goals in
+  match goals with
+  | [] ->
+     let env = Global.env () in
+     let sigma = Evd.from_env env in
+     return (f env sigma)
+  | [goal] ->
+     let* goal in
+     let env = Proofview.Goal.env goal in
+     let sigma = Proofview.Goal.sigma goal in
+     return (f env sigma)
+  | _ :: _ ->
+     fail More_than_one_goal
---
+++ src/api/tactics.mli
@@ -0,0 +1,27 @@
+type 'a tactic = 'a Proofview.tactic
+(** The tactic monad. *)
+
+val return : 'a -> 'a tactic
+(** “Unit” operation of the tactic monad. *)
+
+val ( let* ) : 'a tactic -> ('a -> 'b tactic) -> 'b tactic
+(** “Bind” operation of the tactic monad.
+    [let* x = f in y] is equivalent to [bind f (fun x -> y)].
+ *)
+
+val fail : ?info:Exninfo.info -> exn -> 'a tactic
+(** “Zero” operation of the tactic monad:
+    [fail e] creates a tactic that fails with exception [e]. *)
+
+exception More_than_one_goal
+(** Exception thrown when there is more than one goal in focus. *)
+
+val with_env : (Environ.env -> Evd.evar_map -> 'a) -> 'a tactic
+(** [with_env f] executes [f] in the current environment and evar map, and
+    returns the result as a tactic.
+
+    The “current environment” is dependent on the context:
+    - If there are no goals in focus, the current environment is the global environment.
+    - If there is a single goal in focus, the current environment is the goal's environment.
+    - Otherwise, if there is more than one goal in focus, the tactic fails with [More_than_one_goal].
+ *)
