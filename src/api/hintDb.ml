2ebd9e6aafd42edd0eae5d3542b0af031d9ea5a8
Dario Halilovic <dario.halilovic@epfl.ch>
2026-04-19 21:26:17 +0200

    feat: create hint database API

---
+++ src/api/hintDb.ml
@@ -0,0 +1,156 @@
+(** API for manipulating hint databases. *)
+
+open Hints
+open Names
+
+(** User-facing view of [Hints.hint_ast]. *)
+type 'a hint_kind =
+  | Apply of 'a
+  | EApply of 'a
+  | Exact of 'a
+  | Immediate of 'a
+  | Unfold of Evaluable.t
+  | Extern of Pattern.constr_pattern option * Gentactic.glob_generic_tactic
+
+let of_hint_ast = function
+  | Res_pf h -> Apply h
+  | ERes_pf h -> EApply h
+  | Give_exact h -> Exact h
+  | Res_pf_THEN_trivial_fail h -> Immediate h
+  | Unfold_nth h -> Unfold h
+  | Extern (c, tac) -> Extern (c, tac)
+
+module Hint = struct
+  type t = Hints.FullHint.t
+
+  let run hint f =
+    let f ast = f (of_hint_ast ast) in
+    Hints.FullHint.run hint f
+
+  let pattern hint = Hints.FullHint.pattern hint
+
+  let cost hint = Hints.FullHint.priority hint
+  let name hint = Hints.FullHint.name hint
+  let database hint = Hints.FullHint.database hint
+
+  let print hint = Tactics.with_env (fun env sigma -> Hints.FullHint.print env sigma hint)
+end
+
+(** Type of named hint databases.
+    Unfortunately [Hint_db] does not expose the name of the database, so we have
+    to carry it around. *)
+type t =
+  { name: string;
+    db: Hint_db.t }
+
+let get_db name = { name; db = Hints.searchtable_map name }
+
+let create ?(local = false) ?(discriminated = true) name =
+  Hints.create_hint_db local name TransparentState.full discriminated;
+  get_db name
+
+let transparent_state { db } = Hint_db.transparent_state db
+
+let is_opaque cst db =
+  let ts = transparent_state db in
+  not (TransparentState.is_transparent_constant ts cst)
+
+let set_transparent_state ts { db; name } =
+  let db = Hint_db.set_transparent_state db ts in
+  { db; name }
+
+let set_opaque cst ~opaque db =
+  let open TransparentState in
+  let ts = transparent_state db in
+  let ts = {
+      ts with tr_cst =
+                if opaque then Cpred.remove cst ts.tr_cst
+                else Cpred.add cst ts.tr_cst
+    }
+  in set_transparent_state ts db
+
+let discriminated { db } = Hint_db.use_dn db
+
+let name { name } = name
+
+let locality_or_default locality =
+  match locality with
+  | Some locality -> locality
+  | None ->
+     (* Per the documentation: [Local] is default inside sections, [Export] is
+        the default outside sections. *)
+     if Lib.sections_are_opened () then Local
+     else Export
+
+let add_hint ?locality entry dbname =
+  let locality = locality_or_default locality in
+  Hints.add_hints ~locality [dbname] entry;
+  get_db dbname
+
+let hint_resolve ?locality ?cost ?pattern globref { db; name } =
+  let info = Typeclasses.{ hint_priority = cost; hint_pattern = pattern } in
+  let entry = Hints.HintsResolveEntry [(info, true, globref)] in
+  add_hint ?locality entry name
+
+let hint_cut ?locality regex { db; name } =
+  let entry = Hints.HintsCutEntry regex in
+  add_hint ?locality entry name
+
+let hint_extern ?locality ~cost ?pattern tac { db; name } =
+  let info = Typeclasses.{ hint_priority = Some cost; hint_pattern = pattern } in
+  let entry = Hints.HintsExternEntry (info, tac) in
+  add_hint ?locality entry name
+
+let hint_modes ?locality globref ~modes { db; name } =
+  let modes = Hints.parse_modes modes in
+  let entry = Hints.HintsModeEntry (globref, modes) in
+  add_hint ?locality entry name
+
+let get_modes env globref { db; name } =
+  let modes = Hints.Hint_db.find_mode env globref db in
+  List.map Array.to_list modes
+
+let hint_unfold ?locality globrefs { db; name } =
+  let entry = Hints.HintsUnfoldEntry (List.map Tacred.evaluable_of_global_reference globrefs) in
+  add_hint ?locality entry name
+
+let remove_hints ?locality globrefs { db; name } =
+  let locality = locality_or_default locality in
+  Hints.remove_hints ~locality [name] globrefs;
+  get_db name
+
+let all_hints { db } =
+  let hints = ref [] in
+  Hints.Hint_db.iter (fun _ _ list -> hints := list) db;
+  !hints
+
+let print { db } =
+  Tactics.with_env (fun env sigma -> Hints.pr_hint_db_env env sigma db)
+
+let print_applicable () =
+  let open Tactics in
+  let* goals = Proofview.Goal.goals in
+  match goals with
+  | [] -> CErrors.user_err (Pp.str "No focused goal")
+  | goal :: _ ->
+     let* goal in
+     return ()
+
+let print_reference glob_ref =
+  Tactics.with_env (fun env sigma -> Hints.pr_hint_ref env sigma glob_ref)
+
+let databases () =
+  let database_names = Hints.current_db_names () in
+  CString.Set.fold (fun name dbs -> get_db name :: dbs) database_names []
+
+(** Default databases *)
+
+let core () = get_db "core"
+let arith () = get_db "arith"
+let zarith () = get_db "zarith"
+let bool () = get_db "bool"
+let datatypes () = get_db "datatypes"
+let sets () = get_db "sets"
+let typeclass_instances () = get_db "typeclass_instances"
+let fset () = get_db "fset"
+let ordered_type () = get_db "ordered_type"
---
+++ src/api/hintDb.mli
@@ -0,0 +1,290 @@
+open Names
+
+(** Manipulation of hint databases.
+
+    Hint databases are collections of hints used by proof search tactics such as
+    [auto], [eauto], or [typeclasses eauto].
+
+    @see <https://rocq-prover.org/doc/master/refman/proofs/automatic-tactics/auto.html#hint-databases>
+      Reference Manual, "Hint databases"
+ *)
+
+(** {1 Hints} *)
+
+(** A classification of hints according to the tactic that is performed when the
+    hint is applied. *)
+type 'a hint_kind =
+  | Apply of 'a
+  | EApply of 'a
+  | Exact of 'a
+  | Immediate of 'a
+  | Unfold of Evaluable.t
+  | Extern of Pattern.constr_pattern option * Gentactic.glob_generic_tactic
+
+module Hint : sig
+  type t
+  (** Type of hints. *)
+
+  val run : t -> (Hints.hint hint_kind -> 'a Proofview.tactic) -> 'a Proofview.tactic
+  (** [run hint t] runs tactic [t] on [hint]. *)
+
+  val pattern : t -> Pattern.constr_pattern option
+  (** [pattern hint] returns the pattern that the conclusion of the goal must
+      match for the hint to be applicable.
+
+      Patterns can be specified in the {v Hint Extern v} and {v Hint Resolve v}
+      vernacular commands. *)
+
+  val cost : t -> int
+  (** [cost hint] returns the cost of the hint, as either specified by the user,
+      or the default value. *)
+
+  val name : t -> GlobRef.t option
+  (** [name hint] returns the name of the hint, if any.
+
+      Hints registered through {v Hint Extern v} do not get a name. *)
+
+  val database : t -> string option
+  (** [database hint] returns the name of the database associated to the hint. *)
+
+  val print : t -> Pp.t Proofview.tactic
+  (** [print hint] prints the hint in the current environment. *)
+end
+
+(** {1 Hint databases} *)
+
+type t
+(** Type of hint databases. *)
+
+val create : ?local:bool -> ?discriminated:bool -> string -> t
+(** [create ?local ?discriminated name] creates a new empty hint database with
+    the given name.
+
+    @param local
+      If true, the hint database is local to the current module.
+      Defaults to [false].
+
+    @param discriminated
+      If true, the hint database uses a discrimination net
+      to improve performance. Defaults to [true].
+ *)
+
+val discriminated : t -> bool
+(** [discriminated db] returns [true] if the database uses a discrimination
+    net. *)
+
+val transparent_state : t -> TransparentState.t
+(** [transparent_state db] returns the transparency state of the database.
+
+    @see [TransparentState]
+ *)
+
+val set_transparent_state : TransparentState.t -> t -> t
+(** [set_transparent_state state db] sets the transparency state of the
+    database.
+
+    Previous calls to [set_opaque] are overriden by this function.
+ *)
+
+val is_opaque : Constant.t -> t -> bool
+(** [is_opaque c db] returns [true] if the constant [c] is marked as opaque
+    in the database.
+
+    Opaque constants are not unfolded during proof search, which may improve
+    performance (fewer unfoldings) at the cost of preventing some hints from
+    matching.
+
+    @see <https://rocq-prover.org/doc/master/refman/proofs/automatic-tactics/auto.html#rocq:cmd.Hint-Transparent>
+      Reference manual, "Hint Opaque"
+ *)
+
+val set_opaque : Constant.t -> opaque:bool -> t -> t
+(** [set_opaque c ~opaque db] sets the transparency of the constant in
+    the hint database.
+
+    If [opaque] is [true], [c] becomes opaque, so that the constant is no longer
+    unfolded in the conclusion of the goal when matching hints.
+
+    If [opaque] is [false], [c] is transparently unfolded during unification.
+
+    @see <https://rocq-prover.org/doc/master/refman/proofs/automatic-tactics/auto.html#rocq:cmd.Hint-Transparent>
+      Reference manual, "Hint Opaque"
+ *)
+
+val name : t -> string
+(** [name db] returns the name of the hint database. *)
+
+(** {2 Querying hints} *)
+
+val all_hints : t -> Hint.t list
+(** [all_hints db] returns the list of all hints of the database. *)
+
+(*
+  val hints_without_pattern : t -> Hint.t list
+  (** [hints_without_pattern db] returns the list of all hints of the database which have no pattern. *)
+
+  val hints_referencing : GlobRef.t -> t -> Hint.t list
+(** [hints_referencing glob_ref db] returns the list of all hints associated to
+    the reference in the database [db]. *)
+                                          *)
+
+(** {2 Adding or removing hints} *)
+
+val hint_resolve : ?locality:Hints.hint_locality -> ?cost:int -> ?pattern:(Id.Set.t * Pattern.constr_pattern) -> GlobRef.t -> t -> t
+(** [hint_resolve ?locality ?cost ?pattern qualid db] behaves like the vernacular
+    {v [locality] Hint Resolve qualid | cost pattern : db. v}.
+
+    @param locality
+      Locality of the hint (local, global, export).
+
+    @param cost
+      Cost of the tactic. A lower cost will have higher priority and
+      thus will be tried more frequently.
+
+    @param pattern
+      Pattern that must be matched for the hint to apply.
+
+    @param qualid
+      The qualified name of the definition to resolve.
+
+    @param db
+      Hint database to add the hint to.
+ *)
+
+
+val hint_extern : ?locality:Hints.hint_locality -> cost:int -> ?pattern:(Id.Set.t * Pattern.constr_pattern) -> Gentactic.glob_generic_tactic -> t -> t
+(** [hint_extern ?locality ~cost ?pattern tac db] behaves like the vernacular
+    {v [locality] Hint Extern cost pattern => tactic : db v. v}.
+
+    @param locality
+      Locality of the hint (local, global, export).
+
+    @param cost
+      Cost of the tactic. A lower cost will have higher priority and
+      thus will be tried more frequently.
+
+    @param pattern
+      Pattern that must be matched for the hint to apply.
+
+    @param tac
+      Tactic to perform when the optional pattern is matched.
+
+    @param db
+      Hint database to add the hint to.
+
+    @see <https://rocq-prover.org/doc/master/refman/proofs/automatic-tactics/auto.html#rocq:cmd.Hint-Extern>
+      Reference Manual, "Hint Extern"
+ *)
+
+val hint_cut : ?locality:Hints.hint_locality -> Hints.hints_path -> t -> t
+(** [hint_cut ?locality regex db] behaves like the vernacular
+    {v [locality] Hint Cut [ regex ] : db. v}.
+
+    @param locality
+      Locality of the hint (local, global, export).
+
+    @param regex
+      Regex to add to the cut expression of the database.
+
+    @param db
+      Hint database to add the hint to.
+
+    @see <https://rocq-prover.org/doc/master/refman/proofs/automatic-tactics/auto.html#rocq:cmd.Hint-Cut>
+      Reference Manual, "Hint Cut"
+ *)
+
+val hint_modes : ?locality:Hints.hint_locality -> GlobRef.t -> modes:string -> t -> t
+(** [hint_modes ?locality qualid ~modes db] behaves like the vernacular command
+    {v [locality] Hint Mode qualid modes : db v}.
+
+    @param locality
+      Locality of the hint (local, global, export).
+
+    @param qualid
+      Identifier to set the modes for.
+
+    @param modes
+      Modes of resolution of the identifier.
+
+    @param db
+      Hint database to add the hint to.
+
+    @see <https://rocq-prover.org/doc/master/refman/proofs/automatic-tactics/auto.html#rocq:cmd.Hint-Mode>
+      Reference Manual, "Hint Mode"
+ *)
+
+val get_modes : Environ.env -> GlobRef.t -> t -> Hints.hint_mode list list
+(** [get_modes env qualid db] returns the list of mode declarations of [qualid] in
+    database [db]. *)
+
+val hint_unfold : ?locality:Libobject.locality -> Names.GlobRef.t list -> t -> t
+(** [hint_unfold ?locality qualids db] behaves like the vernacular command
+    {v [locality] Hint Unfold qualids : db. v}.
+
+    @param locality
+      Locality of the hint (local, global, export).
+
+    @param qualids
+      List of identifiers to unfold.
+
+    @param db
+      Hint database to add the hint to.
+
+    @see <https://rocq-prover.org/doc/master/refman/proofs/automatic-tactics/auto.html#rocq:cmd.Hint-Unfold>
+      Reference Manual, "Hint Unfold"
+ *)
+
+val remove_hints : ?locality:Libobject.locality -> Names.GlobRef.t list -> t -> t
+(** [remove_hints ~locality qualids db] removes hints associated to [qualids] in
+    database [db]. *)
+
+(** {2 Printing} *)
+
+val print : t -> Pp.t Proofview.tactic
+(** [print db] prints all entries of the hint database in the current
+    environment. *)
+
+val print_reference : GlobRef.t -> Pp.t Proofview.tactic
+(** [print_reference glob_ref] prints all entries from all hints databases that
+    mention the given qualified name. *)
+
+(** {2 Database registry} *)
+
+val get_db : string -> t
+(** [get_db name] returns the database with the given name.
+
+    @raise [Not_found] if no such database with the given name exist. *)
+
+val databases : unit -> t list
+(** [databases ()] returns the list of registered databases. *)
+
+(** {3 Databases defined in the Rocq standard library} *)
+
+val core : unit -> t
+(** A special database that is automatically used by [auto]. *)
+
+val arith : unit -> t
+(** Database containing lemmas about Peano's arithmetic. *)
+
+val zarith : unit -> t
+(** Database containing lemmas about binary signed integers. *)
+
+val bool : unit -> t
+(** Database containing lemmas about booleans. *)
+
+val datatypes : unit -> t
+(** Database containing lemmas about lists, streams, etc. *)
+
+val sets : unit -> t
+(** Database containing lemmas about sets and relations. *)
+
+val typeclass_instances : unit -> t
+(** A special database containing all typeclass instances declared in the
+    environment. *)
+
+val fset : unit -> t
+(** Internal database for the implementation of the FSets library. *)
+
+val ordered_type : unit -> t
+(** Database containing lemmas about ordered types, mainly used in the FSets and
+    FMaps libraries. *)
