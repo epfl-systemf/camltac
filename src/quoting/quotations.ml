98efc764dbcf8aca03371aa92e69725c3a406e35
Dario Halilovic <dario.halilovic@epfl.ch>
2026-04-20 14:48:02 +0200

    feat: create Quoting PPX

---
+++ src/quoting/quotations.ml
@@ -0,0 +1,68 @@
+(** Registration of PPX rewriters for quoting Rocq code. *)
+
+open Ppxlib
+
+let constrexpr_expansion ~ctxt s =
+  let loc = Expansion_context.Extension.extension_point_loc ctxt in
+  let s = Ast_builder.Default.estring ~loc s in
+  [%expr let open Api in Parsing.parse_constrexpr [%e s]]
+
+let glob_constr_expansion ~ctxt s =
+  let loc = Expansion_context.Extension.extension_point_loc ctxt in
+  let s = Ast_builder.Default.estring ~loc s in
+  [%expr let open Api in Parsing.glob_constr_of_string [%e s]]
+
+let constr_expansion ~ctxt s =
+  let loc = Expansion_context.Extension.extension_point_loc ctxt in
+  let s = Ast_builder.Default.estring ~loc s in
+  [%expr let open Api in Parsing.constr_of_string [%e s]]
+
+let open_constr_expansion ~ctxt s =
+  let loc = Expansion_context.Extension.extension_point_loc ctxt in
+  let s = Ast_builder.Default.estring ~loc s in
+  [%expr let open Api in Parsing.open_constr_of_string [%e s]]
+
+let vernac_expansion ~ctxt s =
+  let loc = Expansion_context.Extension.extension_point_loc ctxt in
+  let s = Ast_builder.Default.estring ~loc s in
+  [%expr let open Api in Parsing.parse_vernac [%e s]]
+
+let expr =
+  Extension.V3.declare
+    "expr"
+    Extension.Context.expression
+    Ast_pattern.(single_expr_payload (estring __))
+    constrexpr_expansion
+
+let preterm =
+  Extension.V3.declare
+    "preterm"
+    Extension.Context.expression
+    Ast_pattern.(single_expr_payload (estring __))
+    glob_constr_expansion
+
+let constr =
+  Extension.V3.declare
+    "constr"
+    Extension.Context.expression
+    Ast_pattern.(single_expr_payload (estring __))
+    constr_expansion
+
+let open_constr =
+  Extension.V3.declare
+    "open_constr"
+    Extension.Context.expression
+    Ast_pattern.(single_expr_payload (estring __))
+    open_constr_expansion
+
+let vernac =
+  Extension.V3.declare
+    "vernac"
+    Extension.Context.expression
+    Ast_pattern.(single_expr_payload (estring __))
+    vernac_expansion
+
+let () =
+  Ppxlib.Driver.register_transformation
+    ~extensions:[expr; preterm; constr; open_constr; vernac]
+    "mltac"
