39f7177cf4c3b5661fe68ca97417a6937b58c9b6
Dario Halilovic <dario.halilovic@epfl.ch>
2026-05-03 23:13:58 +0200

    feat(examples): simplify reification example

--- examples/HashconsedReification.v
+++ examples/HashconsedReification.v
@@ -82,31 +82,6 @@ Section MLReification.
     open Tac2externals
     open Tac2ffi
 
-    let plugin_name = "mltac.plugin.runtime"
-    let pname s = { Tac2expr.mltac_plugin = plugin_name; mltac_tactic = s }
-    
-    let define s = define (pname s)
-    
-    let ref_to_constr ref =
-      EConstr.of_constr (UnivGen.constr_of_monomorphic_global (Global.env ()) ref)
-    let constr_of_qualid n =
-      let qualid = Libnames.qualid_of_string n in
-      lazy (let ref = Nametab.locate qualid in ref_to_constr ref)
-    
-    let bool_typ = constr_of_qualid "bool"
-    let trueb    = constr_of_qualid "true"
-    let falseb   = constr_of_qualid "false"
-    let andb     = constr_of_qualid "andb"
-    let orb      = constr_of_qualid "orb"
-    let negb     = constr_of_qualid "negb"
-    
-    let bool_expr = constr_of_qualid "bool_expr"
-    let bool_expr_Literal = constr_of_qualid "Literal"
-    let bool_expr_Var = constr_of_qualid "Var"
-    let bool_expr_Neg = constr_of_qualid "Neg"
-    let bool_expr_And = constr_of_qualid "And"
-    let bool_expr_Or = constr_of_qualid "Or"
-    
     (* Let us define our own data type in OCaml, and convert it from/to constr when necessary. *)
     type bool_expr =
       | Literal of bool
@@ -121,44 +96,44 @@ Section MLReification.
       or_cons: bool_expr -> bool_expr -> bool_expr;
     }
     
-    let rec quote factories evd t =
-      match EConstr.kind evd t with
-      | Construct _ -> factories.literal_cons (EConstr.eq_constr evd t @@ Lazy.force trueb)
-      | App (head, args) when head = Lazy.force negb && Array.length args = 1 ->
-         let arg = quote factories evd args.(0) in
-         factories.neg_cons arg
-      | App (head, args) when Array.length args = 2 ->
-         let left = quote factories evd args.(0) in
-         let right = quote factories evd args.(1) in
-         let op =
-           if head = Lazy.force andb then factories.and_cons
-           else if head = Lazy.force orb then factories.or_cons
-           else failwith "Unknown boolean function."
-         in op left right
-      | Var ident ->
-         failwith "Variables aren't supported, because I don't want to convert OCaml strings to Rocq strings by hand :("
-      | _ -> CErrors.user_err (Pp.str "Unrecognized term.")
+    let rec quote factories t =
+      match%pat t with
+      | "true" -> return (factories.literal_cons true)
+      | "false" -> return (factories.literal_cons false)
+      | "negb ?x" ->
+         let* x = quote factories x in
+         return (factories.neg_cons x)
+      | "andb ?x ?y" ->
+         let* left = quote factories x in
+         let* right = quote factories y in
+         return (factories.and_cons left right)
+      | "orb ?x ?y" ->
+         let* left = quote factories x in
+         let* right = quote factories y in
+         return (factories.or_cons left right)
+      | _ -> user_error (Pp.str "Unrecognized term.")
     
     let rec unquote e =
-      let make cons args =
-        let cons = Lazy.force cons in
-        EConstr.mkApp (cons, args)
-      in
       match e with
-      | Literal true -> make bool_expr_Literal [| Lazy.force trueb |]
-      | Literal false -> make bool_expr_Literal [| Lazy.force falseb |]
-      | Neg e' -> make bool_expr_Neg [| unquote e' |]
-      | And (e1, e2) -> make bool_expr_And [| unquote e1; unquote e2 |]
-      | Or (e1, e2) -> make bool_expr_Or [| unquote e1; unquote e2 |]
-    
+      | Literal true -> [%constr "Literal true"]
+      | Literal false -> [%constr "Literal false"]
+      | Neg e' ->
+        let* e' = unquote e' in [%constr "Neg %{e'}"]
+      | And (e1, e2) ->
+        let* e1 = unquote e1 in
+        let* e2 = unquote e2 in
+        [%constr "And %{e1} %{e2}"]
+      | Or (e1, e2) ->
+        let* e1 = unquote e1 in
+        let* e2 = unquote e2 in
+        [%constr "Or %{e1} %{e2}"]
+
     (* Our reification function is parametrized by how we construct new applied terms.
        For simple reification, taking [factories = default_factories] works.
        If we want hashconsing, we must use a memoizing variant. *)
     let reify factories t =
-      let env = Global.env () in
-      let evd = Evd.from_env env in
-      let t = Termops.strip_head_cast evd t in
-      unquote (quote factories evd t)
+      let* quoted = quote factories t in
+      unquote quoted
     
     let default_factories = {
         literal_cons = (fun b -> Literal b);
@@ -179,8 +154,8 @@ Section MLReification.
         or_cons = (fun a b -> get_cached (Or (a, b)));
       }
     
-    let () = define "reify" (constr @-> ret constr) @@ (reify default_factories)
-    let () = define "hashcons_reify" (constr @-> ret constr) @@ (reify memoized_factories)
+    let () = Runtime.Registry.register_ltac2 "reify" (constr @-> tac constr) @@ (reify default_factories)
+    let () = Runtime.Registry.register_ltac2 "hashcons_reify" (constr @-> tac constr) @@ (reify memoized_factories)
   }}.
 
   (** A function that reifies the given boolean term using OCaml. *)
@@ -212,27 +187,3 @@ Completely instant!
 
   Abort.
 End MLReification.
-
-(*|
-
-Notes
-=====
-
-What we wish for is roughly the following embed OCaml tactic:
-
-.. code:: ocaml
-
-   let make cons ?args =
-     (* Hashconsing... *)
-
-   let rec reify t =
-     match t with
-     | {%pat|true|} => make "Literal" [%constr true]
-     | {%pat|false|} => make "Literal" [%constr false]
-     | {%pat|negb ?x|} => make "Neg" [| reify x |]
-     | {%pat|andb ?x ?y|} => make "And" [| reify x; reify y |]
-     | {%pat|orb ?x ?y|} => make "Or" [| reify x; reify y |]
-|*)
-
-
-
