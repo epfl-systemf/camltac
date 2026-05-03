a46052cff408a5ed576474e1bf3e1971ce86e1df
Dario Halilovic <dario.halilovic@epfl.ch>
2026-05-03 15:27:36 +0200

    feat: create Ppx_utils.with_let_bindings

--- src/quoting/ppx_utils.ml
+++ src/quoting/ppx_utils.ml
@@ -7,3 +7,11 @@ let rec expr_of_list ~loc = function
   | head :: tail ->
      let tail_expr = expr_of_list ~loc tail in
      [%expr [%e head] :: [%e tail_expr]]
+
+let rec with_let_bindings ~loc bindings expr =
+  match bindings with
+  | [] -> expr
+  | (name, binding) :: rest ->
+     let expr = with_let_bindings ~loc rest expr in
+     let name = Ast_builder.Default.ppat_var ~loc:name.loc name in
+     [%expr let [%p name] = [%e binding] in [%e expr]]
--- src/quoting/ppx_utils.mli
+++ src/quoting/ppx_utils.mli
@@ -5,3 +5,10 @@ open Ppxlib
 val expr_of_list : loc:location -> expression list -> expression
 (** [expr_of_list ~loc list] constructs a single list expression from the given list
     of expressions. *)
+
+val with_let_bindings : loc:location -> (string loc * expression) list -> expression -> expression
+(** [with_let_bindings ~loc bindings expr] wraps expression [expr] with the
+    given list of named let-bindings.
+
+    For example, [with_let_bindings ~loc [({ txt = "x"; … }, [%expr 0]); ({ txt = "y"; … }; [%expr 1])] [%expr x + y]]
+    generates the code [let x = 0 in let y = 1 in x + y]. *)
