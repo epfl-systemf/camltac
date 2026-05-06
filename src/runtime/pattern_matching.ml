24f6445d773d156ca8d037470fd88c8134783ac4
Dario Halilovic <dario.halilovic@epfl.ch>
2026-05-06 16:53:55 +0200

    fix: correctly propagate error in pattern matching branches

--- src/runtime/pattern_matching.ml
+++ src/runtime/pattern_matching.ml
@@ -7,19 +7,20 @@ and pattern = Constrexpr.constr_expr
 and 'a continuation = substitution -> 'a tactic
 and substitution = Ltac_pretype.patvar_map
 
+let matching_error = (CErrors.UserError Pp.(str "No matching clauses for match."), Exninfo.null)
 
 let match_term t ~cases =
   with_env begin fun env sigma ->
-    let rec test_cases = function
-      | [] -> fail (Pp.str "Pattern matching failed.")
+    let rec test_cases (e, info) = function
+      | [] -> Proofview.tclZERO ~info e
       | (pattern, k) :: rest ->
          let _, pattern = Constrintern.interp_constr_pattern env sigma pattern in
          try
            let subst = Constr_matching.matches env sigma pattern t in
            let tac = k subst in
-           Proofview.tclOR tac (fun _ -> test_cases rest)
+           Proofview.tclOR tac (fun e -> test_cases e rest)
          with Constr_matching.PatternMatchingFailure ->
-           test_cases rest
+           test_cases (e, info) rest
     in
-    test_cases cases
+    test_cases matching_error cases
   end
