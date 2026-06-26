ca6170faafd8e5fdbfa6dd164ffd137767800c46
Dario Halilovic <dario.halilovic@epfl.ch>
2026-06-26 13:20:53 +0200

    tests: add test for module backtracking

---
+++ tests/Backtrack.v
@@ -0,0 +1,12 @@
+From Camltac Require Import Camltac.
+
+(** To test backtracking, we use [Restart] in proof-mode instead of [Undo]
+    because [Undo] says "cannot undo". *)
+Goal True.
+  Camltac Module M := ocaml:{{let x = 1}}.
+  Camltac Eval ocaml:{{ return M.x }}.
+  Restart.
+  Camltac Module M := ocaml:{{let x = 2}}.
+  Camltac Eval ocaml:{{ return M.x }}.
+  exact I.
+Qed.
