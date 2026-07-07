d12ed750448365b10fbdccfc9504e6010fc91aaa
Clément Pit-Claudel <clement.pit-claudel@epfl.ch>
2026-07-07 14:08:08 +0200

    fix(tests): Silence the `Backtrack` warning

--- tests/Backtrack.v
+++ tests/Backtrack.v
@@ -2,6 +2,8 @@ From Camltac Require Import Camltac.
 
 (** To test backtracking, we use [Restart] in proof-mode instead of [Undo]
     because [Undo] says "cannot undo". *)
+Set Warnings "-undo-batch-mode".
+
 Goal True.
   Camltac Module M := ocaml:{{let x = 1}}.
   Camltac Eval ocaml:{{ return M.x }}.
