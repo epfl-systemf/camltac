bd922ec8e9763f47408123660bd50b1a538f60bb
Dario Halilovic <dario.halilovic@epfl.ch>
2026-09-14 21:53:13 +0200

    chore: improve types in Build_files

--- src/build_files.mli
+++ src/build_files.mli
@@ -2,28 +2,28 @@
 
 (** {1 Build directories} *)
 
-val root_dir : string
+val root_dir : CUnix.physical_path
 (** [root_dir] is the parent of [build_dir]. *)
 
-val build_dir : string
+val build_dir : CUnix.physical_path
 (** [build_dir] is the path of the [.camltac] directory. *)
 
-val snippets_dir : string
+val snippets_dir : CUnix.physical_path
 (** [snippets_dir] is the path of the directory that stores snippets. *)
 
-val modules_dir : string
+val modules_dir : CUnix.physical_path
 (** [modules_dir] is the path of the directory that stores modules. *)
 
 (** {1 Write methods} *)
 
-val write_snippet : string -> string
+val write_snippet : string -> CUnix.physical_path
 (** [write_snippet contents] saves the contents of the snippet
     to a fresh file in [snippets_dir]. *)
 
-val write_module : string -> string
+val write_module : string -> CUnix.physical_path
 (** [write_module contents] saves the contents of the module
     to a fresh file in [modules_dir]. *)
 
-val write_ppx_driver : string -> string
+val write_ppx_driver : string -> CUnix.physical_path
 (** [write_ppx_driver contents] saves the contents of the given PPX driver to a
     fresh file. *)
